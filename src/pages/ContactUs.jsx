15b12da7e3c02ef40309322ba7b23d3c2e3ac207
antor10655gh <creativefreelancerantor@gmail.com>
2024-11-22 11:13:17 +0600

    add some change on pageheader image

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import PageHeader from "../components/PageHeader/PageHeader";
 import ContactSectionOne from "../components/contact/ContactSectionOne";
 import ContactMap from "../components/contact/ContactMap";
+import { useLocation } from "react-router-dom";
 
 const ContactUs = () => {
+  const location = useLocation();
+
+  const path = location.pathname.split("/").pop();
   return (
     <>
-      <PageHeader title="Contact Us" imageUrl="/images/pageheader.jpg" />
+      <PageHeader title="Contact Us" imageUrl={`/images/${path}.jpg`} />
       <ContactSectionOne />
       <ContactMap />
     </>
